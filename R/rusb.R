328ded637031dc7c063d254b65c290b5dafd41b7
Stephen Carnagua <sacarnagua@gmail.com>
2014-11-10 13:45:44 -0800

    Update rusb.R
    changed lines 64 & 74 from:
          ind <- as.numeric(vardep != flearn)
    to:
          ind <- as.numeric(vardep[[1]] != flearn)

--- R/rusb.R
+++ R/rusb.R
@@ -61,7 +61,7 @@ rusb <- function (formula, data, boot = FALSE, iters = 100, coeflearn = "Breiman
       flearn <- predict(fit, newdata = data, type = "class")
       # errors are a simple count of wrong predictions. 
       # This is called "pseudo loss" because its calculated on all observations, including ones which weren't in the model training.
-      ind <- as.numeric(vardep != flearn)
+      ind <- as.numeric(vardep[[1]] != flearn)
       # create overall error metric, which is simply vector multiplication of (observation weight)*(1 or 0)
       err <- as.numeric(w %*% ind)           
     }
@@ -71,7 +71,7 @@ rusb <- function (formula, data, boot = FALSE, iters = 100, coeflearn = "Breiman
       fit <- rpart(formula = formula, data = tmp.sample, 
                    weights = inner.tmp.weights, control = control)
       flearn <- predict(fit, newdata = data, type = "class")
-      ind <- as.numeric(vardep != flearn)
+      ind <- as.numeric(vardep[[1]] != flearn)
       err <- as.numeric(w %*% ind)           
     }
     
